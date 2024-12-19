7f21859dd5478a74652d04fd1e7ba9d307365f9b
Randall LaPoint, Jr. <rplapointjr@gmail.com>
2024-12-18 20:39:25 -0600

    implemented modular code for adding additional buttons later

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,14 @@ import React from "react";
 
 const Button = ({ label, type, icon, onClick }) => {
   return (
-    <button className={`button ${type}`} onClick={onClick}>
+    <button
+      onClick={onClick}
+      className={`flex items-center justify-center h-12 rounded-lg text-white ${
+        type === "operator"
+          ? "bg-gradient-to-b from-blue-600 via-blue-500 to-blue-600 hover:from-blue-500"
+          : "bg-gradient-to-b from-gray-600 via-gray-500 to-gray-600 hover:from-gray-500"
+      }`}
+    >
       {icon || label}
     </button>
   );
--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,14 +1,12 @@
 import React, { useState } from "react";
-import { numberButtons, operatorButtons } from "@/utils/constants";
 import Display from "./Display";
-import NumberButton from "./NumberButton";
-import OperatorButton from "./OperatorButton";
+import Button from "./Button";
+import { BUTTONS } from "@/utils/constants";
 import {
   appendToExpression,
   resetCalculator,
   calculateResult,
 } from "@/utils/functions";
-import { BUTTONS } from "@/utils/constants";
 
 const Calculator = () => {
   const [expression, setExpression] = useState("");
@@ -33,36 +31,27 @@ const Calculator = () => {
   return (
     <div className="flex flex-col bg-gradient-to-bl from-black via-gray-800 to-black w-[400px] h-fit rounded-xl p-6 gap-6 shadow-lg shadow-black/50">
       <Display expression={expression} result={result} />
-      <div className="flex flex-row w-full gap-4">
-        <div className="w-2/3 grid grid-cols-3 gap-2">
-          {numberButtons.map((button) => (
-            <NumberButton
-              key={button}
-              button={button}
-              onClick={handleButtonClick}
-            />
-          ))}
-        </div>
-        <div className="w-1/3 flex flex-col gap-2">
-          {operatorButtons.map((button) => (
-            <OperatorButton
-              key={button}
-              button={button}
-              onClick={handleButtonClick}
-            />
-          ))}
-        </div>
+      <div className="grid grid-cols-4 gap-2">
+        {BUTTONS.map((button) => (
+          <Button
+            key={button.label}
+            label={button.label}
+            icon={button.icon}
+            onClick={() => handleButtonClick(button.label)}
+            type={button.type}
+          />
+        ))}
       </div>
       <div className="flex flex-row gap-4">
         <button
           onClick={handleClear}
-          className="w-1/2 hover:scale-105 active:scale-100 hover:bg-gradient-to-br bg-gradient-to-b from-blue-600 via-blue-500 to-blue-600 rounded-lg h-12 text-white"
+          className="w-1/2 hover:scale-105 active:scale-100 hover:bg-gradient-to-br bg-gradient-to-b from-red-600 via-red-500 to-red-600 rounded-lg h-12 text-white"
         >
           Clear
         </button>
         <button
           onClick={handleCalculation}
-          className="w-1/2 hover:scale-105 active:scale-100 hover:bg-gradient-to-br bg-gradient-to-b from-gray-600 via-gray-500 to-gray-600 rounded-lg h-12 text-white"
+          className="w-1/2 hover:scale-105 active:scale-100 hover:bg-gradient-to-br bg-gradient-to-b from-blue-600 via-blue-500 to-blue-600 rounded-lg h-12 text-white"
         >
           =
         </button>
